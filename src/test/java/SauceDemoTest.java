db82009e69a694a789c236866fe7f5dca905fa7c
milkani <nikita@milka.me>
2025-04-28 20:55:05 +0300

    [master]: Add progress

--- src/test/java/SauceDemoTest.java
+++ src/test/java/SauceDemoTest.java
@@ -1,3 +1,5 @@
+import lv.acodemy.page_object.InventoryPage;
+import lv.acodemy.page_object.LoginPage;
 import org.assertj.core.api.Assertions;
 import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -8,19 +10,22 @@ import org.testng.annotations.Test;
 public class SauceDemoTest {
 
     ChromeDriver driver;
+    LoginPage loginPage;
+    InventoryPage inventoryPage;
 
     @BeforeMethod
     public void beforeTest() {
         driver = new ChromeDriver();
         driver.get("https://saucedemo.com");
 
-        driver.findElement(By.id("user-name")).sendKeys("standard_user");
-        driver.findElement(By.name("password")).sendKeys("secret_sauce");
-        driver.findElement(By.xpath("//input[@data-test='login-button']")).click();
+        loginPage = new LoginPage(driver);
+        inventoryPage = new InventoryPage(driver);
     }
 
     @Test
     public void verifyLoggedInTest() {
+        loginPage.authorize("standard_user", "secret_sauce");
+
         String productsText = driver.findElement(By.className("title")).getText();
         Assertions.assertThat(productsText)
                 .withFailMessage("Are u lalka? Expected title to be 'Products' but was '%s'", productsText)
@@ -29,6 +34,17 @@ public class SauceDemoTest {
                 .startsWith("Prod")
                 .endsWith("ucts")
                 .isEqualTo("Products");
+    }
+
+    @Test
+    public void logInTest() {
+        loginPage.authorize("standard_user", "secret_sauce");
+    }
+
+    @Test
+    public void addItemToTheCart() {
+        loginPage.authorize("standard_user", "secret_sauce");
+        inventoryPage.addItemToCartByName("Onesie");
     }
 
     @AfterMethod()
