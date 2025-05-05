1b27d37607beb06e3b3d88e8d41bb47758942a2a
nikitam <nikita.milka@citadele.lv>
2025-05-05 20:58:31 +0300

    [master]: Add progress

--- src/test/java/SauceDemoTest.java
+++ src/test/java/SauceDemoTest.java
@@ -1,17 +1,19 @@
-import lv.acodemy.page_object.CartPage;
-import lv.acodemy.page_object.HeaderPage;
-import lv.acodemy.page_object.InventoryPage;
-import lv.acodemy.page_object.LoginPage;
+import com.github.javafaker.Faker;
+import lv.acodemy.page_object.*;
 import org.assertj.core.api.Assertions;
 import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.util.HashMap;
 import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class SauceDemoTest {
 
@@ -22,6 +24,11 @@ public class SauceDemoTest {
     InventoryPage inventoryPage;
     HeaderPage headerPage;
     CartPage cartPage;
+    CheckoutPage checkoutPage;
+
+    private static final Logger logger = LoggerFactory.getLogger(SauceDemoTest.class);
+
+    Faker data = new Faker();
 
     @BeforeMethod
     public void beforeTest() {
@@ -35,6 +42,7 @@ public class SauceDemoTest {
 
         options.addArguments("--incognito");
         options.addArguments("--start-maximized");
+        options.addArguments("--headless");
 
         driver = new ChromeDriver(options);
         driver.get("https://saucedemo.com");
@@ -43,14 +51,15 @@ public class SauceDemoTest {
         inventoryPage = new InventoryPage(driver);
         headerPage = new HeaderPage(driver);
         cartPage = new CartPage(driver);
+        checkoutPage = new CheckoutPage(driver);
     }
 
-    @Test
+    @Test(enabled = false)
     public void verifyLoggedInTest() {
         loginPage.authorize("standard_user", "secret_sauce");
 
         String productsText = driver.findElement(By.className("title")).getText();
-        Assertions.assertThat(productsText)
+        assertThat(productsText)
                 .withFailMessage("Are u lalka? Expected title to be 'Products' but was '%s'", productsText)
                 .isNotNull()
                 .isNotEmpty()
@@ -59,23 +68,41 @@ public class SauceDemoTest {
                 .isEqualTo("Products");
     }
 
-    @Test
+    @Test(enabled = false)
     public void logInTest() {
         loginPage.authorize("standard_user", "secret_sauce");
     }
 
-    @Test
+    @Test(testName = "Add item to the cart")
     public void addItemToTheCart() {
+        logger.info("Scenario: Add item to the cart");
+
+        logger.info("Step 1: User is trying to authorize");
         loginPage.authorize("standard_user", "secret_sauce");
+        logger.info("User is authorized");
+
+        logger.info("Step 2: Adding item to the cart");
         inventoryPage.addItemToCartByName("Onesie");
-        Assertions.assertThat(headerPage.getCartBadgeText()).isEqualTo("1");
+        logger.info("Item added to the cart");
+
+        logger.info("Step 3: Checking if item was added to the cart");
+        assertThat(headerPage.getCartBadgeText()).isEqualTo("1");
+        logger.info("Item was added to the cart");
 
         headerPage.getShoppingCartLink().click();
-        Assertions.assertThat(cartPage.getCartItems().size()).isEqualTo(1);
+        assertThat(cartPage.getCartItems().size()).isEqualTo(1);
 
         cartPage.getCheckoutButton().click();
 
-        System.out.println("123");
+        String firstName = data.name().firstName();
+        String lastName = data.name().lastName();
+        String postalCode = data.address().zipCode();
+
+        logger.debug("First name: {}, last name: {}, postal code: {}", firstName, lastName, postalCode);
+        checkoutPage.fillCheckoutForm(
+                firstName,
+                lastName,
+                postalCode);
 
     }
 
